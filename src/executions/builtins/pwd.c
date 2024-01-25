441b1e03b01891fd94a16cf05d59594ef0a76d0e
Zk <zklim95@hotmail.com>
2024-01-25 19:33:44 +0800

    feat: cd and pwd

--- src/executions/builtins/pwd.c
+++ src/executions/builtins/pwd.c
@@ -1,23 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   pwd.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: zhlim <zhlim@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/23 15:36:22 by zhlim             #+#    #+#             */
-/*   Updated: 2024/01/24 17:34:08 by zhlim            ###   ########.fr       */
+/*   Created: 2024/01/24 17:04:24 by zhlim             #+#    #+#             */
+/*   Updated: 2024/01/25 17:11:49 by zhlim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
+#include "../../includes/executions/builtins/pwd.h"
 
-# include "executions/execve.h"
-# include "executions/builtins/echo.h"
-# include "executions/builtins/pwd.h"
-# include "executions/builtins/cd.h"
-
-void	execute_cmd(t_cmd *cmd);
-
-#endif
+void	ft_pwd(void)
+{
+	char	*cwd;
+	
+	cwd = getcwd(NULL, 0);
+	if (!cwd)
+		return ;
+	printf("%s\n", cwd);
+	free(cwd);
+}
